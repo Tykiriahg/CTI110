c719d2ba1baefb922456b7a1af754c2c97be41c2
Tykiriahg <gastont0328@student.faytechcc.edu>
2022-11-01 14:27:37 -0400

    Update P4HW1_GASTONT.py

--- P4HW1_GASTONT.py
+++ P4HW1_GASTONT.py
@@ -1,7 +1,23 @@
-#CTI 110
-#P4HW1
+#CTI110
+#P4WH1
 #GASTONT
 #11/1/2022
 
+#ASK THE USER FROR 6 GRADES FOR THE 6 MODULES.
 
-gra
+grades = []
+for grade in range(6):
+    grade = int(input("Enter grade: "))
+    grades.append(grade)
+
+print("The grades are: ",grades)
+
+
+print("Highest grade: ", max(grades))
+print("Lowest grade: ", min(grades))
+
+total = sum(grades)
+count = len(grades)
+average = total / count
+print("Toal is: ", total)
+print("Average is: ",average)
